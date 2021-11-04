d9411aa18beb977f0638839485d675b702202a69
Bryan Wheeler <bryan.wheeler@t-3.com>
2021-11-04 00:11:37 -0500

    WIP

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
-function App() {
+const measureValues = [
+  "Kelvin",
+  "Celsius",
+  "Fahrenheit",
+  "Rankine",
+  "Liters",
+  "Tablespoons",
+  "Cubic-Inches",
+  "Cups",
+  "Cubic-Feet",
+  "Gallons",
+]
+
+const ValueInput = ({label, value, setValue}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>{label}</label>
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+    </>
+  )
+}
+
+const ValueDropdown = ({label, setSelectedValue}) => {
+  return (
+    <>
+      <div>{label}</div>
+      <select onSelect={(e) => setSelectedValue(e.currentTarget.value)}>
+        {measureValues.map(v => <option value={v}>{v}</option>)}
+      </ select>
+    </>
+  )
+}
+
+function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [inputMeasurement, setInputMeasurement] = useState(undefined)
+  const [targetMeasurement, setTargetMeasurement] = useState(undefined)
+  const [studentResponse, setStudentResponse] = useState('')
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <ValueInput
+            label={"Input Numerical Value"}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+          />
+          <ValueDropdown
+            label={"Input Unit of Measure"}
+            setSelectedValue={setInputMeasurement}
+          />
+          <ValueDropdown
+            label={"Target Unit of Measure"}
+            setSelectedValue={setTargetMeasurement}
+          />
+          <ValueInput
+            label={"Student Response"}
+            inputValue={studentResponse}
+            setInputValue={setStudentResponse}
+          />
+        </header>
+      </div>
+      <div>
+        <button type="button">Click Me!</button>
+      </div>
+    </>
   );
 }
 
